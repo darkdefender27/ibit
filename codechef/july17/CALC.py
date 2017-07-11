f1a53a7e30b2284aed584b1115bd70da02fc7ce4
Shubham Utwal <shubhamutwal@gmail.com>
2017-07-11 13:10:29 +0530

    Updated CALC.py

--- codechef/july17/CALC.py
+++ codechef/july17/CALC.py
@@ -6,19 +6,16 @@ def main():
     for _ in xrange(T):
         n, b = map(int, raw_input().strip().split(' '))
         result = 0
-        if n%b == 0:
-            aux_size = (n/b)
-            if aux_size%2 == 0:
-                c_sum = b*(aux_size/2)
-                c_mult = (aux_size/2)
-                result = c_sum * c_mult
-            else:
-                aux_ceil = (aux_size/2) + 1
-                c_sum = aux_ceil*b
-                c_mult = aux_size - aux_ceil
-                result = c_sum * c_mult
+        aux_size = (n/b)
+        if aux_size%2 == 0:
+            c_sum = b*(aux_size/2)
+            c_mult = (aux_size/2)
+            result = c_sum * c_mult
         else:
-            result = (n/b) * (n%b)
+            aux_ceil = (aux_size/2) + 1
+            c_sum = aux_ceil*b
+            c_mult = aux_size - aux_ceil
+            result = c_sum * c_mult
 
         print int(result)
 
